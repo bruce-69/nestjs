aa7796b007ca30e62d73c4a8673dda2c12ca6295
jorge <eduardcrow555@gmail.com>
2023-02-14 21:27:55 -0300

    login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,18 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
-import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards
+} from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { AuthService } from "./auth.service";
 import { LoginResponseDto } from "./dto/login-response.dto";
-import { LoginDto } from "./dto/login.dto";
+import { logindto } from "./dto/login.dto";
+
 
 @Controller("auth")
 @ApiTags("auth")
@@ -14,7 +24,17 @@ export class AuthController {
   @ApiOperation({
     summary: "Realizar login, recebendo um token de autenticação"
   })
-  login(@Body() loginDto: LoginDto): Promise<LoginResponseDto> {
+  login(@Body() loginDto: logindto): Promise<LoginResponseDto> {
     return this.authService.login(loginDto);
+  }
+
+  @Get()
+  @UseGuards(AuthGuard())
+  @ApiOperation({
+    summary: "Retorna o usuário autenticado no momento"
+  })
+  @ApiBearerAuth()
+  profile() {
+    return { message: "Autenticação bem sucedida" };
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,18 @@
-import { PrismaModule } from './../prisma/prisma.module';
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { prisma } from '@prisma/client';
+import { Module } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
+import { PrismaModule } from "src/prisma/prisma.module";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: "24h" }
+    })
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService]
 })
-export class AuthModule {}
+export class AuthModule {} 
